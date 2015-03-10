a30953914b597c37056033a69ec41c68d9f0ba51
Janardan Yri <janardan.yri@me.com>
2015-03-10 10:58:01 -0700

    Take out storyboard and spin up our first view controller programmatically.

--- ProgrammaticUIDemo/AppDelegate.swift
+++ ProgrammaticUIDemo/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     // Override point for customization after application launch.
+
+    self.window = UIWindow()
+    self.window?.makeKeyAndVisible()
+
+    let initialViewController = UIViewController()
+    self.window?.rootViewController = initialViewController
+
     return true
   }
 
